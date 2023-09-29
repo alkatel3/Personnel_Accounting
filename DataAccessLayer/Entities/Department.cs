35d0be2f990f12ba15b8aa78ebebf75cac94cc83
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-09-29 11:19:40 +0300

    Implemented Department and Employee entites

---
+++ DataAccessLayer/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Department : BaseEntity<int>
+    {
+        [Required]
+        [DisplayName("Назва")]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Employee> Employees { get; set; }
+
+        public Department()
+        {
+            Employees = new List<Employee>();
+        }
+    }
+}
--- DataAccessLayer/Entities/Employee.cs
+++ DataAccessLayer/Entities/Employee.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Entities
 {
-    internal class Employee
+    public class Employee : BaseEntity<int>
     {
+        [Required]
+        [DisplayName("Прізвище")]
+        public string LastName { get; set; }
+        [Required]
+        [DisplayName("Ім'я")]
+        public string FirstName { get; set; }
+        [DisplayName("По-батькові")]
+        public string? MiddleName { get; set; }
+        [DisplayName("Рік народження")]
+        public uint? BirthYear { get; set; }
+        [DisplayName("Освіта")]
+        public string? Education { get; set; }
+        [Required]
+        [DisplayName("Посада")]
+        public string Position { get; set; }
+        [Required]
+        [DisplayName("Оклад")]
+        public decimal Salaty { get; set; }
+        [DisplayName("Підпорядкований")]
+        public Employee? Supervisor { get; set; }
+        public int DepartmentId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Department? Department { get; set; }
+
     }
 }
